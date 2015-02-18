83d936d1aa2dd656e5c874ad4dbef722b5e05205
elger <elgervb@gmail.com>
2015-02-18 21:02:28 +0100

    formatting

--- imagemanipulation/PresetFilter.php
+++ imagemanipulation/PresetFilter.php
@@ -2,43 +2,65 @@
 namespace imagemanipulation;
 
 use imagemanipulation\ImageBuilder;
-use imagemanipulation\filter\ImageFilterContrast;
+
 class PresetFilter
 {
-    public static function cool(ImageBuilder $builder){
+
+    public static function cool(ImageBuilder $builder)
+    {
         $builder->meanremove()->contrast(50);
     }
-    
-    public static function light(ImageBuilder $builder){
+
+    public static function light(ImageBuilder $builder)
+    {
         $builder->brightness(10)->colorize('#643200', 10);
     }
-    
-    public static function boost(ImageBuilder $builder){
+
+    public static function boost(ImageBuilder $builder)
+    {
         $builder->contrast(35)->brightness(20);
     }
-    
-    public static function deep(ImageBuilder $builder){
+
+    public static function deep(ImageBuilder $builder)
+    {
         $builder->colorize('#73AaBE', 100)->contrast(20);
     }
-    
-    public static function grayscaleDark(ImageBuilder $builder){
+
+    public static function grayscaleDark(ImageBuilder $builder)
+    {
         $builder->grayscale()->contrast(15);
     }
-    
-    public static function sepia(ImageBuilder $builder){
-        $builder->grayscale()->brightness(-10)->contrast(20)->colorize('#3C1E00');
+
+    public static function sepia(ImageBuilder $builder)
+    {
+        $builder->grayscale()
+            ->brightness(- 10)
+            ->contrast(20)
+            ->colorize('#3C1E00');
     }
-    
-    public static function retro(ImageBuilder $builder){
-        
-        $builder->noise(40)->selectiveBlur()->selectiveBlur()->colorize('#704214', 65)->contrast(20)->brightness(10);
+
+    public static function retro(ImageBuilder $builder)
+    {
+        $builder->noise(40)
+            ->selectiveBlur()
+            ->selectiveBlur()
+            ->colorize('#704214', 65)
+            ->contrast(20)
+            ->brightness(10);
     }
-    
-    public static function vintage(ImageBuilder $builder){
-        $builder->colorize('#FF0000', 125)->colorize('#FF9966', 60)->gammaCorrection(1.7, 1.0)->contrast(20);
+
+    public static function vintage(ImageBuilder $builder)
+    {
+        $builder->colorize('#FF0000', 125)
+            ->colorize('#FF9966', 60)
+            ->gammaCorrection(1.7, 1.0)
+            ->contrast(20);
     }
-    
-    public static function blueish(ImageBuilder $builder){
-        $builder->colorize('#222b6d', 60)->colorize('#f7daae', 110)->gammaCorrection(1.2, 1.0);
+
+    public static function blueish(ImageBuilder $builder)
+    {
+        $builder->colorize('#222b6d', 60)
+            ->colorize('#f7daae', 110)
+            ->gammaCorrection(1.2, 1.0);
     }
 }
