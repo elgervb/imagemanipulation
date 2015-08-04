901ef293cd4c637b70a1e7848b8ad16a58ee93dc
Elger van Boxtel <elgervb@gmail.com>
2015-08-04 15:22:01 +0200

    fix travis error
    Fatal error: Method PHPUnit_Framework_ExceptionWrapper::__toString()
    must not throw an exception in
    phar:///home/travis/.phpenv/versions/5.4.37/bin/phpunit/phpunit/TextUI/ResultPrinter.php
    on line 0
    
    https://github.com/sebastianbergmann/phpunit/issues/1598

--- tests/PHPUnitBootstrap.php
+++ tests/PHPUnitBootstrap.php
@@ -7,8 +7,5 @@ function loader($class)
     }else if(realpath(__DIR__ . '/../' . $file)){
       require_once realpath(__DIR__ . '/../' . $file);
     }
-    else{
-        throw new \Exception('could not find class ' . __DIR__ . '/../' . $file);
-    }
 }
 spl_autoload_register('loader');
