944fe07c818dee6edd4977c1ae2fc20e472a472c
elger <elgervb@gmail.com>
2015-08-30 22:11:26 +0200

    add notifications when tests finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var gulp = require('gulp'),
     notify  = require('gulp-notify'),
-    phpunit = require('gulp-phpunit');
+    phpunit = require('gulp-phpunit')
+    _       = require('lodash');
  
 gulp.task('phpunit', function() {
-    var options = {debug: false, notify: true};
+    var options = {debug: false, notify: true, stderr: true};
     gulp.src('phpunit.xml')
         .pipe(phpunit('', options))
-        .on('error', notify.onError({
-            title: "Failed Tests!",
-            message: "Error(s) occurred during testing..."
-        }));
+        .on('error', notify.onError(notification('fail', 'phpunit')))
+        .pipe(notify(notification('pass', 'phpunit')));
 });
  
 gulp.task('default', ['phpunit'], function(){
     gulp.watch('./**/*.php', function(file){
-        gulp.run('phpunit');
+        gulp.start('phpunit');
     });
 });
+
+function notification(status, pluginName, override) {
+    var options = {
+        title:   ( status == 'pass' ) ? 'Tests Passed' : 'Tests Failed',
+        message: ( status == 'pass' ) ? '\n\nAll tests have passed!\n\n' : '\n\nOne or more tests failed...\n\n',
+        icon:    __dirname + '/node_modules/gulp-' + pluginName +'/assets/test-' + status + '.png'
+    };
+    options = _.merge(options, override);
+  return options;
+}
