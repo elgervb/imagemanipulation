88a06f9c5e0b0a187dc6e7c2ba9ab45ddf337f24
Elger van Boxtel <elgervb@gmail.com>
2015-09-01 10:17:52 +0200

    hsl calculation methods
    #15

--- imagemanipulation/color/ColorUtil.php
+++ imagemanipulation/color/ColorUtil.php
@@ -47,6 +47,107 @@ class ColorUtil
 		return new Color($index);
 	}
 	
+	public static function rgb2hsl($r, $g, $b) {
+	    $var_r = $r / 255;
+	    $var_g = $g / 255;
+	    $var_b = $b / 255;
+	    
+	    $var_min = min($var_r,$var_g,$var_b);
+	    $var_max = max($var_r,$var_g,$var_b);
+	    $del_max = $var_max - $var_min;
+	    
+	    $l = ($var_max + $var_min) / 2;
+	    
+	    if ($del_max == 0)
+	    {
+	        $h = 0;
+	        $s = 0;
+	    }
+	    else
+	    {
+	        if ($l < 0.5)
+	        {
+	            $s = $del_max / ($var_max + $var_min);
+	        }
+	        else
+	        {
+	            $s = $del_max / (2 - $var_max - $var_min);
+	        };
+	    
+	        $del_r = ((($var_max - $var_r) / 6) + ($del_max / 2)) / $del_max;
+	        $del_g = ((($var_max - $var_g) / 6) + ($del_max / 2)) / $del_max;
+	        $del_b = ((($var_max - $var_b) / 6) + ($del_max / 2)) / $del_max;
+	    
+	        if ($var_r == $var_max)
+	        {
+	            $h = $del_b - $del_g;
+	        }
+	        elseif ($var_g == $var_max)
+	        {
+	            $h = (1 / 3) + $del_r - $del_b;
+	        }
+	        elseif ($var_b == $var_max)
+	        {
+	            $h = (2 / 3) + $del_g - $del_r;
+	        };
+	    
+	        if ($h < 0)
+	        {
+	            $h += 1;
+	        };
+	    
+	        if ($h > 1)
+	        {
+	            $h -= 1;
+	        };
+	    };
+	    $HSL = $l + ($s << 0x8) + ($h << 0x10);
+	    return array(round($h, 2), round($s, 2), round($l, 2));
+	}
+	
+	/**
+	 * Convert HEX color into HSL
+	 * @param unknown $hex
+	 * @return multitype:number
+	 */
+	public static function hex2hsl($hex) {
+	    $hex = str_replace("#", "", $hex);
+        // Split input by color
+        $hex = str_split($hex, 2);
+        // Convert color values to value between 0 and 1
+        $r = (hexdec($hex[0]));
+        $g = (hexdec($hex[1]));
+        $b = (hexdec($hex[2]));
+        
+        return self::rgb2hsl($r,$g,$b);
+    }
+    
+    /**
+     * Convert HSL into RGB
+     * 
+     * @param int $h
+     * @param int $s
+     * @param int $l
+     * 
+     * @return array 
+     */
+	public static function hsl2rgb($h, $s, $l) {
+        $m2 = ( $l <= 0.5 ) ? $l * ( $s + 1 ) : $l + $s - $l * $s;
+        $m1 = $l * 2 - $m2;
+    
+        $hue = function ( $base ) use ( $m1, $m2 ) {
+            $base = ( $base < 0 ) ? $base + 1 : ( ( $base > 1 ) ? $base - 1 : $base );
+            if ( $base * 6 < 1 ) return round($m1 + ( $m2 - $m1 ) * $base * 6);
+            if ( $base * 2 < 1 ) return round($m2);
+            if ( $base * 3 < 2 ) return round($m1 + ( $m2 - $m1 ) * ( 0.6666666666666666 - $base ) * 6);
+            return round($m1, 1);
+        };
+    
+        return array( $hue( $h + 0.33333 ) * 255, $hue( $h ) * 255, $hue( $h - 0.33333 ) * 255 );
+	}
+	
+	
+	
 	/**
 	 * Returns the color at the specified location of the image resource
 	 *
--- tests/imagemanipulation/color/ColorUtilTest.php
+++ tests/imagemanipulation/color/ColorUtilTest.php
@@ -10,7 +10,7 @@ use imagemanipulation\Coordinate;
  */
 class ColorUtilTest extends \ImagemanipulationTestCase
 {
-    public function testAverage(){
+   public function testAverage(){
         
         $res = $this->getImageRes($this->getOriginalImage(ImageType::JPG), __METHOD__);
         $color = ColorUtil::average($res);
@@ -23,5 +23,27 @@ class ColorUtilTest extends \ImagemanipulationTestCase
         
         $color = ColorUtil::getColorAt($res, Coordinate::create(20,20));
         $this->assertColor($color, 'fe0000');
+    }
+    
+    public function testRgb2hsl(){
+        $this->assertEquals( array(0, 0, 1), ColorUtil::rgb2hsl(255, 255,255), "Error converting rgb white into hsl");
+        $this->assertEquals( array(0, 0, 0), ColorUtil::rgb2hsl(0, 0, 0), "Error converting rgb black into hsl" );
+        $this->assertEquals( array(0.33, 1.0, 0.5), ColorUtil::rgb2hsl(0, 255, 0), "Error converting rgb green into hsl" );
+        $this->assertEquals( array(0.67, 1.0, 0.5), ColorUtil::rgb2hsl(0, 0, 255), "Error converting rgb blue into hsl" );
+    }
+    
+    public function testhsl2Rgb(){
+        $this->assertEquals( array(255, 255,255), ColorUtil::hsl2rgb(0, 0, 1), "Error converting hsl into rgb white ");
+        $this->assertEquals( array(0, 0, 0), ColorUtil::hsl2rgb(0, 0, 0), "Error converting hsl into  rgb black into hsl" );
+        $this->assertEquals( array(0, 255, 0), ColorUtil::hsl2rgb(0.33, 1.0, 0.5), "Error converting hsl into rgb green" );
+        $this->assertEquals( array(0, 0, 255), ColorUtil::hsl2rgb(0.67, 1.0, 0.5), "Error converting hsl into rgb blue" );
+    }
+    
+    public function testHex2hsl(){
+        $this->assertEquals( array(0, 0, 1), ColorUtil::hex2hsl('ffffff'), "Error converting hex white into hsl");
+        $this->assertEquals( array(0, 0, 1), ColorUtil::hex2hsl('#ffffff'), "Error converting hex #white into hsl");
+        $this->assertEquals( array(0, 0, 0), ColorUtil::hex2hsl('000000'), "Error converting hex black into hsl" );
+        $this->assertEquals( array(0.33, 1.0, 0.5), ColorUtil::hex2hsl('00ff00'), "Error converting hex green into hsl" );
+        $this->assertEquals( array(0.67, 1.0, 0.5), ColorUtil::hex2hsl('0000ff'), "Error converting hex blue into hsl" );
     }
 }
