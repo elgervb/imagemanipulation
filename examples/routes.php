de1d67c49e8a005047b740ab00ae6ed6aa12c0db
Elger van Boxtel <elgervb@gmail.com>
2016-01-08 14:33:32 +0100

    serve page with examples

--- examples/routes.php
+++ examples/routes.php
@@ -1,60 +1,112 @@
 <?php
-
 use imagemanipulation\ImageBuilder;
-
 require_once '../vendor/autoload.php';
 
+session_start();
 
+// path without querystring
 $path = $_SERVER["REQUEST_URI"];
-$paths = explode("/", $path);
-$method = str_replace('.png', '', $paths[1]);
+$index = strpos($path, '?');
+if ($index !== false) {
+    $path = substr($path, 0, $index);
+}
 
-$b = ImageBuilder::create(__DIR__ . '/uglydog.png')->thumbPercentage(50);
+// copy image
+$id = sha1(session_id());
+$image = 'tmp/'.$id . '.png';
+if (!is_file($image)) {
+    copy('uglydog.png', $image);
+}
+
 
 if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $path)) {
-    if ($path == "/uglydog.png"){
-        return false;    // serve the requested resource as-is
-    }
-    
-    return call_user_func_array(array($b, $method), array_slice($paths, 2))->render();
-      
+    return false;    // serve the requested resource as-is
 }
-?>
-<p>
-    <a href="brightness.png">brightness</a>
-    <a href="colorize.png">colorize</a>
-    <a href="comic.png">comic</a>
-    <a href="contrast.png">contrast</a>
-    <a href="darken.png">darken</a>
-    <a href="dodge.png">dodge</a>
-    <a href="duotone.png">duotone</a>
-    <a href="edgeDetect.png">edgeDetect</a>
-    <a href="emboss.png">emboss</a>
-    <a href="flip.png">flip</a>
-    <a href="findEdges.png">findEdges</a>
-    <a href="gammaCorrection.png">gammaCorrection</a>
-    <a href="gaussianBlur.png">gaussianBlur</a>
-    <a href="grayscale.png">grayscale</a>
-    <a href="hueRotate.png">hueRotate</a>
-    <a href="meanremove.png">meanremove</a>
-    <a href="motionBlur.png">motionBlur</a>
-    <a href="negative.png">negative</a>
-    <a href="noise.png">noise</a>
-    <a href="oldCardboard.png">oldCardboard</a>
-    <a href="opacity.png">opacity</a>
-    <a href="pixelate.png">pixelate</a>
-    <a href="randomBlocks.png">randomBlocks</a>
-    <a href="rotate.png">rotate</a>
-    <a href="roundedCorners.png">roundedCorners</a>
-    <a href="scatter.png">scatter</a>
-    <a href="selectiveBlur.png">selectiveBlur</a>
-    <a href="semiGrayscale.png">semiGrayscale</a>
-    <a href="sepia.png">sepia</a>
-    <a href="sepiaFast.png">sepiaFast</a>
-    <a href="sharpen.png">sharpen</a>
-    <a href="smooth.png">smooth</a>
-    <a href="sobelEdgeDetect.png">sobelEdgeDetect</a>
-    <a href="sobelEdgeEnhance.png">sobelEdgeEnhance</a>
-    <a href="truecolor.png">truecolor</a>
-    <a href="vignette.png">vignette</a>
-</p>
+
+
+
+$b = ImageBuilder::create(__DIR__ . DIRECTORY_SEPARATOR . $image);
+
+if($path == '/clear') {
+    copy('uglydog.png', $image);
+} else if ($path !== '/') {
+    $paths = explode("/", $path);
+    $method = $paths[1];
+    call_user_func_array(array($b, $method), array_slice($paths, 2))->save(null, true);
+} 
+?><!doctype html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='initial-scale=1'>
+	<title>Aurora</title>
+
+  <style>
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+    }
+    body {
+        display: flex;
+    }
+    a {
+        color: #2CCC87;
+    }
+    .filters {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    .filters li {
+        padding: .1rem 1rem;
+    }
+  </style>
+	
+</head>
+<body>
+    <ul class="filters">
+        <li><a href="/clear">clear</a></li>
+        <li><a href="/brightness">brightness</a></li>
+        <li><a href="/colorize">colorize</a></li>
+        <li><a href="/comic">comic</a></li>
+        <li><a href="/contrast">contrast</a></li>
+        <li><a href="/darken">darken</a></li>
+        <li><a href="/dodge">dodge</a></li>
+        <li><a href="/duotone">duotone</a></li>
+        <li><a href="/edgeDetect">edgeDetect</a></li>
+        <li><a href="/emboss">emboss</a></li>
+        <li><a href="/flip">flip</a></li>
+        <li><a href="/findEdges">findEdges</a></li>
+        <li><a href="/gammaCorrection">gammaCorrection</a></li>
+        <li><a href="/gaussianBlur">gaussianBlur</a></li>
+        <li><a href="/grayscale">grayscale</a></li>
+        <li><a href="/hueRotate">hueRotate</a></li>
+        <li><a href="/meanremove">meanremove</a></li>
+        <li><a href="/motionBlur">motionBlur</a></li>
+        <li><a href="/negative">negative</a></li>
+        <li><a href="/noise">noise</a></li>
+        <li><a href="/oldCardboard">oldCardboard</a></li>
+        <li><a href="/opacity">opacity</a></li>
+        <li><a href="/pixelate">pixelate</a></li>
+        <li><a href="/randomBlocks">randomBlocks</a></li>
+        <li><a href="/rotate">rotate</a></li>
+        <li><a href="/roundedCorners">roundedCorners</a></li>
+        <li><a href="/scatter">scatter</a></li>
+        <li><a href="/selectiveBlur">selectiveBlur</a></li>
+        <li><a href="/semiGrayscale">semiGrayscale</a></li>
+        <li><a href="/sepia">sepia</a></li>
+        <li><a href="/sepiaFast">sepiaFast</a></li>
+        <li><a href="/sharpen">sharpen</a></li>
+        <li><a href="/smooth">smooth</a></li>
+        <li><a href="/sobelEdgeDetect">sobelEdgeDetect</a></li>
+        <li><a href="/sobelEdgeEnhance">sobelEdgeEnhance</a></li>
+        <li><a href="/truecolor">truecolor</a></li>
+        <li><a href="/vignette">vignette</a></li>
+    </ul>
+    <div>
+        <img src="<?= $image?>?time=<?= time()?>" />
+    </div>
+</body>
+</html>
