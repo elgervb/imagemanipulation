bd227bcedf4cefd26c1c982be1d47246316e41cd
Elger van Boxtel <e.vanboxtel@hw986-evanboxtel.local>
2016-11-14 17:27:23 +0100

    Add long running Sepia test

--- imagemanipulation/filter/ImageFilterSepia.php
+++ imagemanipulation/filter/ImageFilterSepia.php
@@ -35,27 +35,34 @@ class ImageFilterSepia implements IImageFilter
 		$dstImg = $aResource->getResource();
 		$imgX = $aResource->getX();
 		$imgY = $aResource->getY();
+		$previous = ["index" => -1, "result" => 0];
 		
 		for ($y = 0; $y < $imgY; $y ++)
 		{
 			for ($x = 0; $x < $imgX; $x ++)
 			{
 				$dstRGB = imagecolorat( $dstImg, $x, $y );
+				if ($previous["index"] === $dstRGB) {
+				    $newRGB = $previous["result"];
+				} else {
+    				$dstR = $dstRGB >> 16 & 0xFF;
+    				$dstG = $dstRGB >> 8 & 0xFF;
+    				$dstB = $dstRGB & 0xFF;
+    				
+    				$newR = ($dstR * 0.393 + $dstG * 0.769 + $dstB * 0.189) - $this->darken;
+    				$newG = ($dstR * 0.349 + $dstG * 0.686 + $dstB * 0.168) - $this->darken;
+    				$newB = ($dstR * 0.272 + $dstG * 0.534 + $dstB * 0.131) - $this->darken;
+    				
+    				$newR = ($newR > 255) ? 255 : (($newR < 0) ? 0 : $newR);
+    				$newG = ($newG > 255) ? 255 : (($newG < 0) ? 0 : $newG);
+    				$newB = ($newB > 255) ? 255 : (($newB < 0) ? 0 : $newB);
+    				
+    				$newRGB = imagecolorallocate( $dstImg, $newR, $newG, $newB );
+    				
+    				$previous["index"] = $dstRGB;
+    				$previous["result"] = $newRGB;
+				}
 				
-				//$dstA = ($dstRGB >> 24) << 1;
-				$dstR = $dstRGB >> 16 & 0xFF;
-				$dstG = $dstRGB >> 8 & 0xFF;
-				$dstB = $dstRGB & 0xFF;
-				
-				$newR = ($dstR * 0.393 + $dstG * 0.769 + $dstB * 0.189) - $this->darken;
-				$newG = ($dstR * 0.349 + $dstG * 0.686 + $dstB * 0.168) - $this->darken;
-				$newB = ($dstR * 0.272 + $dstG * 0.534 + $dstB * 0.131) - $this->darken;
-				
-				$newR = ($newR > 255) ? 255 : (($newR < 0) ? 0 : $newR);
-				$newG = ($newG > 255) ? 255 : (($newG < 0) ? 0 : $newG);
-				$newB = ($newB > 255) ? 255 : (($newB < 0) ? 0 : $newB);
-				
-				$newRGB = imagecolorallocate( $dstImg, $newR, $newG, $newB );
 				imagesetpixel( $dstImg, $x, $y, $newRGB );
 			}
 		}
