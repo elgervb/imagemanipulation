b0e3d14f17fc203c145ceccf5628be9cd124f832
Elger van Boxtel <e.vanboxtel@hw986-evanboxtel.local>
2016-11-15 14:48:07 +0100

    overlay example
    #48 fixed

---
+++ examples/overlay.php
@@ -0,0 +1,20 @@
+<?php
+
+use imagemanipulation\overlay\ImageFilterOverlay;
+use imagemanipulation\ImageImageResource;
+use imagemanipulation\ImageType;
+
+$resource = new ImageImageResource(new \SplFileInfo(__DIR__ . DIRECTORY_SEPARATOR . 'uglydog.png'));
+$overlayResource = new ImageImageResource(new \SplFileInfo(__DIR__ . DIRECTORY_SEPARATOR . 'monkey.png'));
+
+$overlay = new ImageFilterOverlay(
+    $overlayResource,
+    20,
+    0,
+    0,
+    true
+);
+
+$resource->filter($overlay);
+$resource->imageoutput(null, ImageType::PNG, 80);
+
